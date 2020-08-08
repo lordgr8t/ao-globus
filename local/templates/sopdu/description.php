2a9148612a949d96fe71d6f25658536b24e1df8d
sopdu <info@sopdu.org>
2020-08-09 02:09:26 +0300

    Загрузка болванки

---
+++ local/templates/sopdu/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Шаблон для магазина канцелярии',
+  'DESCRIPTION' => 'Шаблон разработан в Интернет-Лаборатории sopdu (https://sopdu.org)',
+  'SORT' => '',
+  'TYPE' => '',
+);
+?>
